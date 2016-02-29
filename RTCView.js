dc39d13af2ead88b7b1bb0c39a1deccae0ef0ea7
Howard Yang <appdevoney@gmail.com>
2016-02-29 14:27:56 +0800

    v0.3.0

--- RTCView.js
+++ RTCView.js
@@ -1,30 +1,22 @@
 'use strict';
 var React = require('react-native');
-var { requireNativeComponent } = React;
+var { requireNativeComponent, PropTypes } = React;
 
 var RTCView = {
   name: 'RTCVideoView',
   propTypes: {
-    streamURL: React.PropTypes.number,
+    streamURL: PropTypes.number,
   },
 };
 
 var v = requireNativeComponent('RTCVideoView', RTCView, {nativeOnly: {
-  'scaleX': true,
-  'scaleY': true,
-  'testID': true,
-  'decomposedMatrix': true,
-  'backgroundColor': true,
-  'accessibilityComponentType': true,
-  'renderToHardwareTextureAndroid': true,
-  'translateY': true,
-  'translateX': true,
-  'accessibilityLabel': true,
-  'accessibilityLiveRegion': true,
-  'importantForAccessibility': true,
-  'rotation': true,
-  'opacity': true,
-  'onLayout': true,
+  testID: true,
+  accessibilityComponentType: true,
+  renderToHardwareTextureAndroid: true,
+  accessibilityLabel: true,
+  accessibilityLiveRegion: true,
+  importantForAccessibility: true,
+  onLayout: true,
 }});
 
 module.exports = v;
