f96a6189164b4389dc6c0be95dbb43d72d53bbae
Mark Marcantano <m.marcantano@gmail.com>
2016-03-10 12:16:30 -0800

    update module export to support new version of react native 0.19+

--- RCTWebRTC/Classes/RTCVideoViewManager.m
+++ RCTWebRTC/Classes/RTCVideoViewManager.m
@@ -51,7 +51,7 @@ RCT_CUSTOM_VIEW_PROPERTY(streamURL, NSNumber, RTCEAGLVideoView)
   if (json) {
     NSInteger objectID = [json integerValue];
 
-    WebRTCModule *module = self.bridge.modules[@"WebRTCModule"];
+    WebRTCModule *module = [self.bridge moduleForName:@"WebRTCModule"];
     RTCMediaStream *stream = module.mediaStreams[@(objectID)];
 
     if (stream.videoTracks.count) {
