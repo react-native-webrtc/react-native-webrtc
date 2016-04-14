aa1cecc646e6c8c07074957177322561d505f943
zxcpoiu <zxcpoiu@gmail.com>
2016-04-14 17:33:15 +0800

    [js][android] complete media track property and method.
    1. pass correspond properties to js when create track.
    
    2. implement track.enabled = true/false which can be used to mute a track.
    
    3. implement track.stop()
       maybe unnecessary to invoke it if we explicitly release media stream.
       but some implementation would use it when hangup.
       this also supported in chrome and firfox.
    
       I'm not exactly understand what this means, but here are some hints:
       [Google Developers - MediaStream Deprecations](https://developers.google.com/web/updates/2015/07/mediastream-deprecations?hl=en)
       [W3C track.stop() procedure](https://www.w3.org/TR/mediacapture-streams/#dom-mediastreamtrack-stop)
       [W3C detached meaning](https://www.w3.org/TR/mediacapture-streams/#track-detached)
    
       what exaclty `detached` means in doc?

--- MediaStreamTrack.js
+++ MediaStreamTrack.js
@@ -1,11 +1,20 @@
 'use strict';
 
-var React = require('react-native');
-var {
-  NativeModules,
-} = React;
+const EventTarget = require('event-target-shim');
+const WebRTCModule = require('react-native').NativeModules.WebRTCModule;
 
-var WebRTCModule = NativeModules.WebRTCModule;
+const MediaStreamErrorEvent = require('./MediaStreamErrorEvent');
+
+import type MediaStreamError from './MediaStreamError';
+
+const MEDIA_STREAM_TRACK_EVENTS = [
+  'ended',
+  'mute',
+  'unmute',
+  'overconstrained', // --- see: https://www.w3.org/TR/mediacapture-streams/#constrainable-interface
+];
+
+type MediaStreamTrackState = "live" | "ended";
 
 type SourceInfo = {
   id: string;
@@ -19,17 +28,77 @@ class MediaStreamTrack {
     WebRTCModule.mediaStreamTrackGetSources(success);
   }
 
-  enabled: boolean;
+  _enabled: boolean;
   id: number; // NOTE: spec wants string here
   kind: string;
   label: string;
   muted: boolean;
+  readonly: boolean; // how to decide?
+  readyState: MediaStreamTrackState; // readyState in java: INITIALIZING, LIVE, ENDED, FAILED
+  remote: boolean;
+
+  onended: ?Function;
+  onmute: ?Function;
+  onunmute: ?Function;
+  overconstrained: ?Function;
+
   constructor(info) {
+    this._enabled = info.enabled;
     this.id = info.id;
-    this.enabled = true;
     this.kind = info.kind;
     this.label = info.label;
     this.muted = false;
+    this.readonly = true; // how to decide?
+    this.remote = info.remote;
+    this.readyState = (info.readyState === "INITIALIZING"
+                    || info.readyState === "LIVE" ) ? "live" : "ended";
+  }
+
+  get enabled(): boolean {
+    return this._enabled;
+  }
+
+  set enabled(enabled: boolean): void {
+    if (enabled === this._enabled) {
+      return;
+    }
+    WebRTCModule.mediaStreamTrackSetEnabled(this.id, !this._enabled);
+    this._enabled = !this._enabled;
+    this.muted = !this._enabled;
+  }
+
+  stop() {
+    if (this.remote) {
+      return;
+    }
+    WebRTCModule.mediaStreamTrackStop(this.id);
+    this._enabled = false;
+    this.readyState = 'ended';
+    this.muted = !this._enabled;
+  }
+
+  applyConstraints() {
+    throw new Error('Not implemented.');
+  }
+
+  clone() {
+    throw new Error('Not implemented.');
+  }
+
+  getCapabilities() {
+    throw new Error('Not implemented.');
+  }
+
+  getConstraints() {
+    throw new Error('Not implemented.');
+  }
+
+  getSettings() {
+    throw new Error('Not implemented.');
+  }
+
+  clone() {
+    throw new Error('Not implemented.');
   }
 }
 
--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -211,6 +211,9 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
                     trackInfo.putString("id", mediaStreamTrackId + "");
                     trackInfo.putString("label", "Video");
                     trackInfo.putString("kind", track.kind());
+                    trackInfo.putBoolean("enabled", track.enabled());
+                    trackInfo.putString("readyState", track.state().toString());
+                    trackInfo.putBoolean("remote", true);
                     tracks.pushMap(trackInfo);
                 }
                 for (int i = 0; i < mediaStream.audioTracks.size(); i++) {
@@ -221,6 +224,9 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
                     trackInfo.putString("id", mediaStreamTrackId + "");
                     trackInfo.putString("label", "Audio");
                     trackInfo.putString("kind", track.kind());
+                    trackInfo.putBoolean("enabled", track.enabled());
+                    trackInfo.putString("readyState", track.state().toString());
+                    trackInfo.putBoolean("remote", true);
                     tracks.pushMap(trackInfo);
                 }
                 params.putArray("tracks", tracks);
@@ -330,6 +336,9 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
                 trackInfo.putString("id", mediaStreamTrackId + "");
                 trackInfo.putString("label", "Video");
                 trackInfo.putString("kind", videoTrack.kind());
+                trackInfo.putBoolean("enabled", videoTrack.enabled());
+                trackInfo.putString("readyState", videoTrack.state().toString());
+                trackInfo.putBoolean("remote", false);
                 tracks.pushMap(trackInfo);
 
                 mediaStream.addTrack(videoTrack);
@@ -355,6 +364,9 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
             trackInfo.putString("id", mediaStreamTrackId + "");
             trackInfo.putString("label", "Audio");
             trackInfo.putString("kind", audioTrack.kind());
+            trackInfo.putBoolean("enabled", audioTrack.enabled());
+            trackInfo.putString("readyState", audioTrack.state().toString());
+            trackInfo.putBoolean("remote", false);
             tracks.pushMap(trackInfo);
 
             mediaStream.addTrack(audioTrack);
@@ -386,6 +398,32 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
 
         array.pushMap(audio);
         callback.invoke(array);
+    }
+
+    @ReactMethod
+    public void mediaStreamTrackStop(final String id) {
+        final int trackId = Integer.parseInt(id);
+        MediaStreamTrack track = mMediaStreamTracks.get(trackId);
+        if (track == null) {
+            return;
+        }
+        track.setEnabled(false);
+        track.setState(MediaStreamTrack.State.ENDED);
+        mMediaStreamTracks.remove(trackId);
+		// what exaclty `detached` means in doc?
+		// see: https://www.w3.org/TR/mediacapture-streams/#track-detached
+    }
+
+    @ReactMethod
+    public void mediaStreamTrackSetEnabled(final String id, final boolean enabled) {
+        final int trackId = Integer.parseInt(id);
+        MediaStreamTrack track = mMediaStreamTracks.get(trackId);
+        if (track == null) {
+            return;
+        } else if (track.enabled() == enabled) {
+            return;
+        }
+        track.setEnabled(enabled);
     }
 
     public WritableMap getCameraInfo(int index) {
