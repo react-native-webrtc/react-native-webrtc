2a75ca084df1c9bd757ed9477407af329676e47f
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2016-06-30 16:38:27 -0500

    Preserve the aspect ratio of video
    Try to come closer to being in accord with
    https://www.w3.org/TR/html5/embedded-content-0.html#the-video-element:
    
    In the absence of style rules to the contrary, video content should be
    rendered inside the element's playback area such that the video content
    is shown centered in the playback area at the largest possible size that
    fits completely within it, with the video content's aspect ratio being
    preserved. Thus, if the aspect ratio of the playback area does not match
    the aspect ratio of the video, the video will be shown letterboxed or
    pillarboxed. Areas of the element's playback area that do not contain
    the video represent nothing.

--- android/src/main/java/com/oney/WebRTCModule/WebRTCView.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCView.java
@@ -9,6 +9,24 @@ import org.webrtc.VideoRenderer;
 import org.webrtc.VideoRendererGui;
 
 public class WebRTCView extends GLSurfaceView {
+    /**
+     * The scaling type to be utilized by default.
+     *
+     * The default value is in accord with
+     * https://www.w3.org/TR/html5/embedded-content-0.html#the-video-element:
+     *
+     * In the absence of style rules to the contrary, video content should be
+     * rendered inside the element's playback area such that the video content
+     * is shown centered in the playback area at the largest possible size that
+     * fits completely within it, with the video content's aspect ratio being
+     * preserved. Thus, if the aspect ratio of the playback area does not match
+     * the aspect ratio of the video, the video will be shown letterboxed or
+     * pillarboxed. Areas of the element's playback area that do not contain the
+     * video represent nothing.
+     */
+    private static final RendererCommon.ScalingType DEFAULT_SCALING_TYPE
+        = RendererCommon.ScalingType.SCALE_ASPECT_FIT;
+
     private VideoRenderer.Callbacks localRender;
     private VideoRenderer mVideoRenderer;
     private MediaStream mMediaStream;
@@ -22,7 +40,13 @@ public class WebRTCView extends GLSurfaceView {
             }
         });
 
-        RendererCommon.ScalingType scalingType = RendererCommon.ScalingType.SCALE_ASPECT_FILL;
+        RendererCommon.ScalingType scalingType = DEFAULT_SCALING_TYPE;
+        // TODO In order to achieve compliance with
+        // https://www.w3.org/TR/html5/embedded-content-0.html#the-video-element
+        // and the Web and iOS implementations, have a translucent background:
+        //
+        // Areas of the element's playback area that do not contain the video
+        // represent nothing.
         localRender = VideoRendererGui.create(0, 0, 100, 100, scalingType, false);
     }
 
@@ -32,10 +56,10 @@ public class WebRTCView extends GLSurfaceView {
         }
         mMediaStream = mediaStream;
 
-        RendererCommon.ScalingType scalingType = RendererCommon.ScalingType.SCALE_ASPECT_FILL;
-
         mVideoRenderer = new VideoRenderer(localRender);
         mediaStream.videoTracks.get(0).addRenderer(mVideoRenderer);
+
+        RendererCommon.ScalingType scalingType = DEFAULT_SCALING_TYPE;
         VideoRendererGui.update(localRender, 0, 0, 100, 100, scalingType, false);
     }
 }
--- ios/RCTWebRTC/RTCVideoViewManager.m
+++ ios/RCTWebRTC/RTCVideoViewManager.m
@@ -5,30 +5,193 @@
 //  Created by one on 2015/9/25.
 //  Copyright © 2015年 Facebook. All rights reserved.
 //
-
-#import "RTCVideoViewManager.h"
-#import "RTCEAGLVideoView.h"
-#import "RTCVideoTrack.h"
-#import "RTCMediaStream.h"
-#import "WebRTCModule.h"
-
+#import <AVFoundation/AVFoundation.h>
 #import <objc/runtime.h>
 
-@interface UIView (WebRTCModule)
+#import "RTCEAGLVideoView.h"
+#import "RTCMediaStream.h"
+#import "RTCVideoTrack.h"
+#import "RTCVideoViewManager.h"
+#import "WebRTCModule.h"
+
+/**
+ * Implements an equivalent of {@code HTMLVideoElement} i.e. Web's video
+ * element.
+ */
+@interface RTCVideoView : UIView <RTCVideoRenderer, RTCEAGLVideoViewDelegate>
+
+/**
+ * The {@link RTCEAGLVideoView} which implements the actual
+ * {@link RTCVideoRenderer} of this instance and which this instance fits within
+ * itself so that the rendered video preserves the aspect ratio of
+ * {@link #_videoSize}.
+ */
+@property (nonatomic, readonly) RTCEAGLVideoView *subview;
+
+@end
+
+@implementation RTCVideoView {
+  /**
+   * The width and height of the video (frames) rendered by {@link #subview}.
+   */
+  CGSize _videoSize;
+}
+
+/**
+ * Initializes and returns a newly allocated view object with the specified
+ * frame rectangle.
+ *
+ * @param frame The frame rectangle for the view, measured in points.
+ */
+- (instancetype)initWithFrame:(CGRect)frame {
+  if (self = [super initWithFrame:frame]) {
+    RTCEAGLVideoView *subview = [[RTCEAGLVideoView alloc] init];
+
+    subview.delegate = self;
+
+    _videoSize.height = 0;
+    _videoSize.width = 0;
+
+    self.opaque = NO;
+    [self addSubview:subview];
+  }
+  return self;
+}
+
+/**
+ * Lays out the subview of this instance while preserving the aspect ratio of
+ * the video it renders.
+ */
+- (void)layoutSubviews {
+  [super layoutSubviews];
+
+  UIView *subview = self.subview;
+  if (!subview) {
+    return;
+  }
+
+  CGFloat width = _videoSize.width, height = _videoSize.height;
+  CGRect newValue;
+  if (width <= 0 || height <= 0) {
+    newValue.origin.x = 0;
+    newValue.origin.y = 0;
+    newValue.size.width = 0;
+    newValue.size.height = 0;
+  } else {
+    // The implementation is in accord with
+    // https://www.w3.org/TR/html5/embedded-content-0.html#the-video-element:
+    //
+    // In the absence of style rules to the contrary, video content should be
+    // rendered inside the element's playback area such that the video content
+    // is shown centered in the playback area at the largest possible size that
+    // fits completely within it, with the video content's aspect ratio being
+    // preserved. Thus, if the aspect ratio of the playback area does not match
+    // the aspect ratio of the video, the video will be shown letterboxed or
+    // pillarboxed. Areas of the element's playback area that do not contain the
+    // video represent nothing.
+    newValue
+      = AVMakeRectWithAspectRatioInsideRect(
+          CGSizeMake(width, height),
+          self.bounds);
+  }
+
+  CGRect oldValue = subview.frame;
+  if (newValue.origin.x != oldValue.origin.x
+      || newValue.origin.y != oldValue.origin.y
+      || newValue.size.width != oldValue.size.width
+      || newValue.size.height != oldValue.size.height) {
+    subview.frame = newValue;
+  }
+}
+
+/**
+ * Implements the getter of the {@code subview} property of this
+ * {@code RTCVideoView}. Gets the {@link RTCEAGLVideoView} subview of this
+ * {@code RTCVideoView} which implements the actual {@link RTCVideoRenderer} of
+ * this instance and which actually renders {@link #currentRenderer}.
+ *
+ * @returns The {@code RTCEAGLVideoView} subview of this {@code RTCVideoView}
+ * which implements the actual {@code RTCVideoRenderer} of this instance and
+ * which actually renders {@code currentRenderer}.
+ */
+- (RTCEAGLVideoView *)subview {
+  // In order to reduce the number of strong retainments of the RTCEAGLVideoView
+  // instance and, thus, the risk of memory leaks, retrieve the subview from the
+  // super's list of subviews of this view.
+  for (UIView *subview in self.subviews) {
+    if ([subview isKindOfClass:[RTCEAGLVideoView class]]) {
+      return (RTCEAGLVideoView *)subview;
+    }
+  }
+  return nil;
+}
+
+#pragma mark - RTCVideoRenderer methods
+
+/**
+ * Renders a specific video frame. Delegates to the subview of this instance
+ * which implements the actual {@link RTCVideoRenderer}.
+ *
+ * @param frame The video frame to render.
+ */
+- (void)renderFrame:(RTCI420Frame*)frame {
+  id<RTCVideoRenderer> videoRenderer = self.subview;
+  if (videoRenderer) {
+    [videoRenderer renderFrame:frame];
+  }
+}
+
+/**
+ * Sets the size of the video frame to render.
+ *
+ * @param size The size of the video frame to render.
+ */
+- (void)setSize:(CGSize)size {
+  id<RTCVideoRenderer> videoRenderer = self.subview;
+  if (videoRenderer) {
+    [videoRenderer setSize:size];
+  }
+}
+
+#pragma mark - RTCEAGLVideoViewDelegate methods
+
+/**
+ * Notifies this {@link RTCEAGLVideoViewDelegate} that a specific
+ * {@link RTCEAGLVideoView} had the size of the video (frames) it renders
+ * changed.
+ *
+ * @param videoView The {@code RTCEAGLVideoView} which had the size of the video
+ * (frames) it renders changed to the specified size.
+ * @param size The new size of the video (frames) to be rendered by the
+ * specified {@code videoView}.
+ */
+- (void)videoView:(RTCEAGLVideoView *)videoView didChangeVideoSize:(CGSize)size {
+  if (videoView == self.subview) {
+    _videoSize = size;
+
+    __weak UIView *weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+      UIView *strongSelf = weakSelf;
+      [strongSelf setNeedsLayout];
+    });
+  }
+}
+
+@end
+
+@interface RTCVideoView (WebRTCModule)
 
 @property (nonatomic, strong) RTCVideoTrack *currentRenderer;
 
 @end
 
-@implementation UIView (WebRTCModule)
+@implementation RTCVideoView (WebRTCModule)
 
-- (RTCVideoTrack *)currentRenderer
-{
+- (RTCVideoTrack *)currentRenderer {
   return objc_getAssociatedObject(self, _cmd);
 }
 
-- (void)setCurrentRenderer:(RTCVideoTrack *)currentRenderer
-{
+- (void)setCurrentRenderer:(RTCVideoTrack *)currentRenderer {
   objc_setAssociatedObject(self, @selector(currentRenderer), currentRenderer, OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
 
@@ -38,20 +201,17 @@
 
 RCT_EXPORT_MODULE()
 
-- (UIView *)view
-{
-  UIView *v = [[RTCEAGLVideoView alloc] init];
+- (UIView *)view {
+  RTCVideoView *v = [[RTCVideoView alloc] init];
   v.clipsToBounds = YES;
   return v;
 }
 
-- (dispatch_queue_t)methodQueue
-{
+- (dispatch_queue_t)methodQueue {
   return dispatch_get_main_queue();
 }
 
-RCT_CUSTOM_VIEW_PROPERTY(streamURL, NSNumber, RTCEAGLVideoView)
-{
+RCT_CUSTOM_VIEW_PROPERTY(streamURL, NSNumber, RTCVideoView) {
   if (json) {
     NSString *objectID = (NSString *)json;
 
