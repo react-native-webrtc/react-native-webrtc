bd0ae7f46cb426b2e9b12b8259bf33a5a435bcfe
zxcpoiu <zxcpoiu@gmail.com>
2016-12-27 19:01:55 +0800

    android: move CameraEventsHandler from VideoCapturerAndroid to CameraVideoCapturer
    onCameraOpening params changed from int to String

--- android/src/main/java/com/oney/WebRTCModule/CameraEventsHandler.java
+++ android/src/main/java/com/oney/WebRTCModule/CameraEventsHandler.java
@@ -2,9 +2,9 @@ package com.oney.WebRTCModule;
 
 import android.util.Log;
 
-import org.webrtc.VideoCapturerAndroid;
+import org.webrtc.CameraVideoCapturer;
 
-class CameraEventsHandler implements VideoCapturerAndroid.CameraEventsHandler {
+class CameraEventsHandler implements CameraVideoCapturer.CameraEventsHandler {
     private final static String TAG = WebRTCModule.TAG;
 
     // Camera error handler - invoked when camera can not be opened
@@ -22,8 +22,8 @@ class CameraEventsHandler implements VideoCapturerAndroid.CameraEventsHandler {
 
     // Callback invoked when camera is opening.
     @Override
-    public void onCameraOpening(int cameraId) {
-        Log.d(TAG, String.format("CameraEventsHandler.onCameraOpening: cameraId=%s", cameraId));
+    public void onCameraOpening(String cameraName) {
+        Log.d(TAG, String.format("CameraEventsHandler.onCameraOpening: cameraName=%s", cameraName));
     }
 
     // Callback invoked when first camera frame is available after camera is opened.
