3c88c54a01282280280fef5ca06186e418145fbe
zxcpoiu <zxcpoiu@users.noreply.github.com>
2018-08-17 16:26:39 +0800

    RTCUtils: fix coding styles and _deepClone

--- RTCUtil.js
+++ RTCUtil.js
@@ -1,19 +1,24 @@
 'use strict';
 
 /**
+ * Internal util for deep clone object. Object.assign() only does a shallow copy
+ *
+ * @param {Object} obj - object to be cloned
+ * @return {Object} cloned obj
+ */
+function _deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+/**
  * Merge custom constraints with the default one. The custom one take precedence.
  *
  * @param {Object} custom - custom webrtc constraints
  * @param {Object} def - default webrtc constraints
  * @return {Object} constraints - merged webrtc constraints
  */
-
- function _deepClone(obj) {
-  return JSON.parse(JSON.stringify(obj))
-}
-
 export function mergeMediaConstraints(custom, def) {
-  let constraints = _deepClone(def)
+  const constraints = (def ? _deepClone(def) : {});
   if (custom) {
     if (custom.mandatory) {
       constraints.mandatory = {...constraints.mandatory, ...custom.mandatory};
