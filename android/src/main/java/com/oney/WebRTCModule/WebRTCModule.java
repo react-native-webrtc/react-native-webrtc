5b9e9f94235ba1c4b1cdceccb952ed7a43bbfe5c
Jennifer Weber <jennifer@rabb.it>
2019-01-29 21:19:42 +0100

    android, ios: allow (un)muting remote tracks

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -384,6 +384,25 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
         return stream;
     }
 
+    private MediaStreamTrack getTrack(String trackId) {
+        MediaStreamTrack track = getLocalTrack(trackId);
+
+        if (track == null) {
+            for (int i = 0, size = mPeerConnectionObservers.size();
+                    i < size;
+                    i++) {
+                PeerConnectionObserver pco
+                    = mPeerConnectionObservers.valueAt(i);
+                track = pco.remoteTracks.get(trackId);
+                if (track != null) {
+                    break;
+                }
+            }
+        }
+
+        return track;
+    }
+
     MediaStreamTrack getLocalTrack(String trackId) {
         return getUserMediaImpl.getTrack(trackId);
     }
@@ -565,7 +584,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
     }
 
     private void mediaStreamTrackSetEnabledAsync(String id, boolean enabled) {
-        MediaStreamTrack track = getLocalTrack(id);
+        MediaStreamTrack track = getTrack(id);
         if (track == null) {
             Log.d(TAG, "mediaStreamTrackSetEnabled() track is null");
             return;
