7d5c401bc46cf0b17c689457361bbfeacd72ab73
Paul Kompfner <kompfner@gmail.com>
2020-08-15 15:28:45 -0400

    Update `setDailyInCallAudioMode()` on iOS so that it works even if audio isn't playing yet, making automatic audio input/output device selection on iOS work reliably

--- ios/RCTWebRTC/WebRTCModule+Daily.m
+++ ios/RCTWebRTC/WebRTCModule+Daily.m
@@ -7,27 +7,56 @@
 
 #import "WebRTCModule.h"
 
+#import <objc/runtime.h>
 #import <WebRTC/RTCAudioSession.h>
 #import <WebRTC/RTCAudioSessionConfiguration.h>
+
+@interface WebRTCModule (Daily)
+@property (nonatomic, strong) NSTimer *inCallAudioModeRetryTimer;
+@end
 
 @implementation WebRTCModule (Daily)
 
 #pragma mark - setDailyDefaultAudioMode
 
+- (void)setInCallAudioModeRetryTimer:(NSTimer *)inCallAudioModeRetryTimer {
+  objc_setAssociatedObject(self,
+                           @selector(inCallAudioModeRetryTimer),
+                           inCallAudioModeRetryTimer,
+                           OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSTimer *)inCallAudioModeRetryTimer {
+  return  objc_getAssociatedObject(self, @selector(inCallAudioModeRetryTimer));
+}
+
 RCT_EXPORT_METHOD(setDailyInCallAudioMode:(BOOL)setInCallAudioMode) {
-  // For now it doesn't seem like we need to do anything to "unset" the in-call
-  // audio mode, so just return.
+  // Cancel retry timer (if any) for setting the in-call audio mode
+  [self.inCallAudioModeRetryTimer invalidate];
+  
+  // Do nothing if we're attempting to "unset" the in-call audio mode (for now
+  // it doesn't seem like there's anything to do).
   if (!setInCallAudioMode) {
     return;
   }
   
   RTCAudioSession *audioSession = RTCAudioSession.sharedInstance;
   
-  if ([audioSession.category isEqualToString:AVAudioSessionCategoryPlayAndRecord] &&
-      [audioSession.mode isEqualToString:AVAudioSessionModeVideoChat]) {
+  // If audioSession isn't active, configuring it won't work.
+  // Instead, schedule a timer to try again.
+  if (!audioSession.isActive) {
+    self.inCallAudioModeRetryTimer = [NSTimer timerWithTimeInterval:1 repeats:NO block:^(NSTimer * _Nonnull timer) {
+      [self setDailyInCallAudioMode:YES];
+    }];
+    [[NSRunLoop mainRunLoop] addTimer:self.inCallAudioModeRetryTimer
+                              forMode:NSRunLoopCommonModes];
     return;
   }
   
+  [self applyInCallAudioModeToSession:audioSession];
+}
+
+- (void)applyInCallAudioModeToSession:(RTCAudioSession *)audioSession {
   [audioSession lockForConfiguration];
   
   RTCAudioSessionConfiguration *config = [[RTCAudioSessionConfiguration alloc] init];
