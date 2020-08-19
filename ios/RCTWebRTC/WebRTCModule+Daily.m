ff4ee000388b5899e18c6f7794e11fa2d7bf7e67
Paul Kompfner <kompfner@gmail.com>
2020-08-19 11:01:34 -0400

    Add ability to toggle between Daily audio modes (video and voice) on iOS

--- ios/RCTWebRTC/WebRTCModule+Daily.m
+++ ios/RCTWebRTC/WebRTCModule+Daily.m
@@ -11,32 +11,42 @@
 #import <WebRTC/RTCAudioSession.h>
 #import <WebRTC/RTCAudioSessionConfiguration.h>
 
+NSString *const AUDIO_MODE_VIDEO_CALL = @"video";
+NSString *const AUDIO_MODE_VOICE_CALL = @"voice";
+NSString *const AUDIO_MODE_IDLE = @"idle";
+
 @interface WebRTCModule (Daily)
-@property (nonatomic, strong) NSTimer *inCallAudioModeRetryTimer;
+@property (nonatomic, strong) NSTimer *audioModeRetryTimer;
 @end
 
 @implementation WebRTCModule (Daily)
 
-#pragma mark - setDailyDefaultAudioMode
+#pragma mark - setDailyAudioMode
 
-- (void)setInCallAudioModeRetryTimer:(NSTimer *)inCallAudioModeRetryTimer {
+- (void)setAudioModeRetryTimer:(NSTimer *)audioModeRetryTimer {
   objc_setAssociatedObject(self,
-                           @selector(inCallAudioModeRetryTimer),
-                           inCallAudioModeRetryTimer,
+                           @selector(audioModeRetryTimer),
+                           audioModeRetryTimer,
                            OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-- (NSTimer *)inCallAudioModeRetryTimer {
-  return  objc_getAssociatedObject(self, @selector(inCallAudioModeRetryTimer));
+- (NSTimer *)audioModeRetryTimer {
+  return  objc_getAssociatedObject(self, @selector(audioModeRetryTimer));
 }
 
-RCT_EXPORT_METHOD(setDailyInCallAudioMode:(BOOL)setInCallAudioMode) {
+RCT_EXPORT_METHOD(setDailyAudioMode:(NSString *)audioMode) {
+  // Validate input
+  if (![@[AUDIO_MODE_VIDEO_CALL, AUDIO_MODE_VOICE_CALL, AUDIO_MODE_IDLE] containsObject:audioMode]) {
+    NSLog(@"[Daily] invalid argument to setDailyAudioMode: %@", audioMode);
+    return;
+  }
+  
   // Cancel retry timer (if any) for setting the in-call audio mode
-  [self.inCallAudioModeRetryTimer invalidate];
+  [self.audioModeRetryTimer invalidate];
   
   // Do nothing if we're attempting to "unset" the in-call audio mode (for now
   // it doesn't seem like there's anything to do).
-  if (!setInCallAudioMode) {
+  if ([audioMode isEqualToString:AUDIO_MODE_IDLE]) {
     return;
   }
   
@@ -45,23 +55,25 @@ RCT_EXPORT_METHOD(setDailyInCallAudioMode:(BOOL)setInCallAudioMode) {
   // If audioSession isn't active, configuring it won't work.
   // Instead, schedule a timer to try again.
   if (!audioSession.isActive) {
-    self.inCallAudioModeRetryTimer = [NSTimer timerWithTimeInterval:1 repeats:NO block:^(NSTimer * _Nonnull timer) {
-      [self setDailyInCallAudioMode:YES];
+    self.audioModeRetryTimer = [NSTimer timerWithTimeInterval:1 repeats:NO block:^(NSTimer * _Nonnull timer) {
+      [self setDailyAudioMode:audioMode];
     }];
-    [[NSRunLoop mainRunLoop] addTimer:self.inCallAudioModeRetryTimer
+    [[NSRunLoop mainRunLoop] addTimer:self.audioModeRetryTimer
                               forMode:NSRunLoopCommonModes];
     return;
   }
   
-  [self applyInCallAudioModeToSession:audioSession];
+  [self applyAudioMode:audioMode toSession:audioSession];
 }
 
-- (void)applyInCallAudioModeToSession:(RTCAudioSession *)audioSession {
+- (void)applyAudioMode:(NSString *)audioMode toSession:(RTCAudioSession *)audioSession {
   [audioSession lockForConfiguration];
   
   RTCAudioSessionConfiguration *config = [[RTCAudioSessionConfiguration alloc] init];
   config.category = AVAudioSessionCategoryPlayAndRecord;
-  config.mode = AVAudioSessionModeVideoChat;
+  config.mode = ([audioMode isEqualToString:AUDIO_MODE_VIDEO_CALL] ?
+                 AVAudioSessionModeVideoChat :
+                 AVAudioSessionModeVoiceChat);
   config.categoryOptions = AVAudioSessionCategoryOptionAllowBluetooth;
   
   NSError *error;
@@ -70,7 +82,7 @@ RCT_EXPORT_METHOD(setDailyInCallAudioMode:(BOOL)setInCallAudioMode) {
   [audioSession unlockForConfiguration];
   
   if (error) {
-    NSLog(@"[Daily] setDailyDefaultAudioMode error: %@", error);
+    NSLog(@"[Daily] error applying in-call audio mode %@: %@", audioMode, error);
   }
 }
 
