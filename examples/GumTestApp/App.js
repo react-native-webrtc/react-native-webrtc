5c1d5e038dca99babb779d922ce76f2c874ef8f4
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-11-20 16:54:58 +0100

    misc: add simple example that uses getUserMedia

---
+++ examples/GumTestApp/App.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { mediaDevices, RTCView } from 'react-native-webrtc';
+
+const App: () => React$Node = () => {
+  const [stream, setStream] = useState(null);
+  const start = async () => {
+    console.log('start');
+    if (!stream) {
+      let s;
+      try {
+        s = await mediaDevices.getUserMedia({ video: true });
+        setStream(s);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+  };
+  const stop = () => {
+    console.log('stop');
+    if (stream) {
+      stream.release();
+      setStream(null);
+    }
+  };
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+      {
+        stream &&
+          <RTCView
+            streamURL={stream.toURL()}
+            style={styles.stream} />
+      }
+        <View
+          style={styles.footer}>
+          <Button
+            title = "Start"
+            onPress = {start} />
+          <Button
+            title = "Stop"
+            onPress = {stop} />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: Colors.white,
+    ...StyleSheet.absoluteFill
+  },
+  stream: {
+    flex: 1
+  },
+  footer: {
+    backgroundColor: Colors.lighter,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+});
+
+export default App;
