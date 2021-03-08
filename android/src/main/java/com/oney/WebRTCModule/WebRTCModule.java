266905df2313e86e133fd69972efc26f038c5b8d
altox <xeroxaltox@gmail.com>
2021-03-08 15:32:48 +0300

    wip(android): fix build

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.webrtc.*;
 import org.webrtc.audio.AudioDeviceModule;
