c65ff45cf5c7e7701e399ab406529f9f0966e690
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-04-02 15:07:42 +0545

    fix: update set playback mode function

--- setPlaybackMode.js
+++ setPlaybackMode.js
@@ -1,13 +1,20 @@
 
+import { Platform } from 'react-native';
 import {NativeModules} from 'react-native';
 
-const { WebRTCModule } = NativeModules;
+const { PlaybackModeModule } = NativeModules;
 
 /**
  * Sets the play back mode for webrtc library. 
  * Setting the playback mode disables microphone permission when viewing webrtc stream
  * @param {boolean} isPlaybackMode 
  */
-export function setPlaybackMode(isPlaybackMode) {
-    WebRTCModule.setPlaybackMode(isPlaybackMode);
+export default function setPlaybackMode(isPlaybackMode) {
+    if (Platform.OS === "ios") {
+        if(isPlaybackMode) {
+            PlaybackModeModule.setPlaybackMode();
+        } else {
+            PlaybackModeModule.resetPlaybackMode();
+        } 
+    }
 }
