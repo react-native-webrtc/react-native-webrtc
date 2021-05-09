e36691218142337a709e27432583393fcaf01e3d
Aditya Santoso <aditya.santoso88@gmail.com>
2021-05-09 13:51:11 +0800

    tools: fix downloadBitcode.sh
    PACKAGE_VERSION failed to be populated if $THIS_DIR contains whitespace. Wrap them in double-quotes

--- tools/downloadBitcode.sh
+++ tools/downloadBitcode.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
-PACKAGE_VERSION=$(cat ${THIS_DIR}/../package.json \
+PACKAGE_VERSION=$(cat "${THIS_DIR}/../package.json" \
   | grep "\"version\":" \
   | head -1 \
   | awk -F: '{ print $2 }' \
@@ -12,7 +12,7 @@ PACKAGE_VERSION=$(cat ${THIS_DIR}/../package.json \
 WEBRTC_DL="https://github.com/react-native-webrtc/react-native-webrtc/releases/download/${PACKAGE_VERSION}/WebRTC.tar.xz"
 
 
-pushd ${THIS_DIR}/../apple
+pushd "${THIS_DIR}/../apple"
 
 # Cleanup
 rm -rf WebRTC.xcframework WebRTC.dSYMs
