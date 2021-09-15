a1c2b394a18289a43cd337e36c77b1a0a3ea2236
8BallBomBom <weaver123_johnathon@hotmail.com>
2021-09-15 21:30:14 +0800

    Add required EventEmitter calls.

--- ios/RCTWebRTC/WebRTCModule.m
+++ ios/RCTWebRTC/WebRTCModule.m
@@ -118,4 +118,12 @@ RCT_EXPORT_MODULE();
   ];
 }
 
+RCT_EXPORT_METHOD(addListener : (NSString *)eventName) {
+  // Keep: Required for RN built in Event Emitter Calls.
+}
+
+RCT_EXPORT_METHOD(removeListeners : (NSInteger)count) {
+  // Keep: Required for RN built in Event Emitter Calls.
+}
+
 @end
