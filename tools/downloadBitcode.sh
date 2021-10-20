eedc6aa2b712ce451e616b38f1c760fa69f8dcf3
Saúl Ibarra Corretgé <s@saghul.net>
2021-10-20 23:25:53 +0200

    android,ios: update WebRTC to M94
    Starting now, WebRTC binaries are going to be downloaded at
    post-install, from the specified locations in package.json.

--- tools/downloadBitcode.sh
+++ tools/downloadBitcode.sh
@@ -3,24 +3,12 @@
 set -euo pipefail
 
 THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
-PACKAGE_VERSION=$(cat "${THIS_DIR}/../package.json" \
-  | grep "\"version\":" \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-WEBRTC_DL="https://github.com/react-native-webrtc/react-native-webrtc/releases/download/${PACKAGE_VERSION}/WebRTC.tar.xz"
 
+pushd ${THIS_DIR}
 
-pushd "${THIS_DIR}/../apple"
+export RN_WEBRTC_BITCODE=1
+export RN_WEBRTC_FORCE_DOWNLOAD=1
 
-# Cleanup
-rm -rf WebRTC.xcframework WebRTC.dSYMs
-
-# Download
-echo "Downloading files..."
-echo $PACKAGE_VERSION
-curl -L -s ${WEBRTC_DL} | tar Jxf -
-echo "Done!"
+node downloadWebRTC.js
 
 popd
