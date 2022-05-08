7bd68f40ba5309bf4cc93668217c020b5df3cfe1
Amit Kumar Soni <amit.soni7@gmail.com>
2022-05-08 18:08:08 +0530

    added get trackbyId in impl

--- android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
+++ android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
@@ -171,6 +171,17 @@ class GetUserMediaImpl {
         return private_ == null ? null : private_.track;
     }
 
+    MediaStreamTrack getTrackByType(String type) {
+        for (String key : tracks.keySet()){
+            TrackPrivate private_ = tracks.get(key);
+
+            if(private_.track.kind().equalsIgnoreCase(type)){
+                return private_.track;
+            }
+        }
+        return null;
+    }
+
     /**
      * Implements {@code getUserMedia}. Note that at this point constraints have
      * been normalized and permissions have been granted. The constraints only
