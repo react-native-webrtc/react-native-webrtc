9d730fc7d6dbbd3b28fb867a941c6e6b13d859a1
Hans <admin@itsrv.tw>
2022-10-24 14:01:40 +0800

    Add type check on JS side

--- src/RTCIceCandidate.ts
+++ src/RTCIceCandidate.ts
@@ -1,10 +1,19 @@
-
 export default class RTCIceCandidate {
     candidate: string;
     sdpMLineIndex: number;
     sdpMid: string;
 
     constructor(info) {
+        if (typeof info?.candidate !== 'string') {
+            throw new TypeError('`candidate` must be string');
+        }
+        if (typeof info?.sdpMLineIndex !== 'number') {
+            throw new TypeError('`sdpMLineIndex` must be number');
+        }
+        if (typeof info?.sdpMid !== 'string') {
+            throw new TypeError('`sdpMid` must be string');
+        }
+
         this.candidate = info.candidate;
         this.sdpMLineIndex = info.sdpMLineIndex;
         this.sdpMid = info.sdpMid;
