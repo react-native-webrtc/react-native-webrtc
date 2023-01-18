271c7c2c13952612112ec22f6e7838a06bb5342f
Filipi Fuchter <filipi87@gmail.com>
2023-01-18 08:53:06 -0300

    Fixing code issue after merge

--- ios/RCTWebRTC/WebRTCModule.m
+++ ios/RCTWebRTC/WebRTCModule.m
@@ -115,7 +115,7 @@ RCT_EXPORT_MODULE();
     kEventDataChannelReceiveMessage,
     kEventMediaStreamTrackMuteChanged,
     kEventPeerConnectionOnRemoveTrack,
-    kEventPeerConnectionOnTrack
+    kEventPeerConnectionOnTrack,
     kEventMediaDevicesOnDeviceChange,
   ];
 }
