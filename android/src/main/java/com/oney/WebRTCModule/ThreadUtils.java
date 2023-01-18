5bffe0ae82600202694b6bc2598fe947f34e8a08
Filipi Fuchter <filipi87@gmail.com>
2023-01-18 08:48:42 -0300

    Removing unused import

--- android/src/main/java/com/oney/WebRTCModule/ThreadUtils.java
+++ android/src/main/java/com/oney/WebRTCModule/ThreadUtils.java
@@ -1,7 +1,6 @@
 package com.oney.WebRTCModule;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
