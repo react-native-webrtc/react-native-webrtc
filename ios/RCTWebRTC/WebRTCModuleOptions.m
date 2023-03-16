5bfce40e1b38c1fd5f75dc057a4efb169d65482c
Saúl Ibarra Corretgé <s@saghul.net>
2023-03-16 13:25:03 +0100

    ios: add ability to customize initialization options easily
    Currently supporting:
    
    - (video) codec factories
    - Field trials
    - Logging level

---
+++ ios/RCTWebRTC/WebRTCModuleOptions.m
@@ -0,0 +1,29 @@
+#import "WebRTCModuleOptions.h"
+
+@implementation WebRTCModuleOptions
+
+#pragma mark - This class is a singleton
+
++ (instancetype)sharedInstance {
+    static WebRTCModuleOptions *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+
+    return sharedInstance;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.fieldTrials = nil;
+        self.videoEncoderFactory = nil;
+        self.videoDecoderFactory = nil;
+        self.loggingSeverity = RTCLoggingSeverityNone;
+    }
+
+    return self;
+}
+
+@end
