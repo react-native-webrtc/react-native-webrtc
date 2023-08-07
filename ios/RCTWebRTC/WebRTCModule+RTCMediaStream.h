56f762aa225a5d3b5d1b6ee6bc36248d6aa9c312
montanaeli <emont@dolby.com>
2023-08-07 17:43:06 -0300

    Min fixes

--- ios/RCTWebRTC/WebRTCModule+RTCMediaStream.h
+++ ios/RCTWebRTC/WebRTCModule+RTCMediaStream.h
@@ -1,11 +1,8 @@
 #import "CaptureController.h"
 #import "WebRTCModule.h"
 
-
-#if !TARGET_OS_TV
-    @interface WebRTCModule (RTCMediaStream)
-    - (RTCVideoTrack *)createVideoTrackWithCaptureController:
-        (CaptureController * (^)(RTCVideoSource *))captureControllerCreator;
-    - (NSArray *)createMediaStream:(NSArray<RTCMediaStreamTrack *> *)tracks;
-    @end
-#endif
+@interface WebRTCModule (RTCMediaStream)
+- (RTCVideoTrack *)createVideoTrackWithCaptureController:
+    (CaptureController * (^)(RTCVideoSource *))captureControllerCreator;
+- (NSArray *)createMediaStream:(NSArray<RTCMediaStreamTrack *> *)tracks;
+@end
