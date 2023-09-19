dbbdbb4ac72f750faf11b42ba46ec875691217db
Elisa Montana <108739691+montanaeli@users.noreply.github.com>
2023-09-19 13:38:56 -0300

    tvos: fix disabling media stream tracks

--- ios/RCTWebRTC/WebRTCModule+RTCMediaStream.m
+++ ios/RCTWebRTC/WebRTCModule+RTCMediaStream.m
@@ -374,16 +374,13 @@ RCT_EXPORT_METHOD(mediaStreamTrackRelease : (nonnull NSString *)trackID) {
 }
 
 RCT_EXPORT_METHOD(mediaStreamTrackSetEnabled : (nonnull NSNumber *)pcId : (nonnull NSString *)trackID : (BOOL)enabled) {
-#if TARGET_OS_TV
-    return;
-#else
-
     RTCMediaStreamTrack *track = [self trackForId:trackID pcId:pcId];
     if (track == nil) {
         return;
     }
 
     track.isEnabled = enabled;
+#if !TARGET_OS_TV
     if (track.captureController) {  // It could be a remote track!
         if (enabled) {
             [track.captureController startCapture];
