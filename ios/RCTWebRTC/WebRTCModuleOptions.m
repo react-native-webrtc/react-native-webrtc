6e0365c60fc27997233dc4820454876875d6b19b
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2023-11-27 15:02:40 +0100

    Revert "Revert "ios: add ability to customize RTCAudioDevice""
    This reverts commit d06ab571331c801f773ea32610437964e8c07210.

--- ios/RCTWebRTC/WebRTCModuleOptions.m
+++ ios/RCTWebRTC/WebRTCModuleOptions.m
@@ -17,6 +17,7 @@
 
 - (instancetype)init {
     if (self = [super init]) {
+        self.audioDevice = nil;
         self.fieldTrials = nil;
         self.videoEncoderFactory = nil;
         self.videoDecoderFactory = nil;
