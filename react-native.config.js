5bedfbdb3e155241e26323612115cd826fc8f534
Johnathon Weaver <weaver123_johnathon@hotmail.com>
2024-04-11 13:50:14 +0800

    Add the RTCVideoView to the interop layer

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,20 @@
 'use strict';
 
+module.exports = {
+    project: {
+        android: {
+            unstable_reactLegacyComponentNames: [ 'RTCVideoView' ]
+        },
+        ios: {
+            unstable_reactLegacyComponentNames: [ 'RTCVideoView' ]
+        }
+    }
+};
+
 const macSwitch = '--use-react-native-macos';
 
 if (process.argv.includes(macSwitch)) {
     process.argv = process.argv.filter(arg => arg !== macSwitch);
     process.argv.push('--config=metro.config.macos.js');
-    module.exports = {
-        reactNativePath: 'node_modules/react-native-macos'
-    };
+    module.exports.reactNativePath = 'node_modules/react-native-macos';
 }
