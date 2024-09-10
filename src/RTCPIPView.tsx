4033e8b731dacf5915729f5959396a1bca0e5969
davidliu <davidliu@deviange.net>
2024-09-11 01:30:08 +0900

    lint fixes

--- src/RTCPIPView.tsx
+++ src/RTCPIPView.tsx
@@ -1,8 +1,7 @@
 import { Component, forwardRef } from 'react';
+import ReactNative, { UIManager } from 'react-native';
 
 import RTCView, { RTCIOSPIPOptions, RTCVideoViewProps } from './RTCView';
-// @ts-ignore
-import ReactNative, { UIManager } from 'react-native';
 
 export interface RTCPIPViewProps extends RTCVideoViewProps {
   iosPIP?: RTCIOSPIPOptions & {
@@ -13,37 +12,35 @@ export interface RTCPIPViewProps extends RTCVideoViewProps {
 /**
  * A convenience wrapper around RTCView to handle the fallback view as a prop.
  */
-const RTCPIPView = forwardRef((props: RTCPIPViewProps, ref) => {
-  const rtcViewProps = { ...props };
-  const fallbackView = rtcViewProps.iosPIP?.fallbackView;
+const RTCPIPView = forwardRef<Component, RTCPIPViewProps>((props, ref) => {
+    const rtcViewProps = { ...props };
+    const fallbackView = rtcViewProps.iosPIP?.fallbackView;
 
-  delete rtcViewProps.iosPIP?.fallbackView;
+    delete rtcViewProps.iosPIP?.fallbackView;
 
-  return (
-    // @ts-ignore
-    <RTCView ref={ref} 
-      {...rtcViewProps}>
-      {fallbackView}
-    </RTCView>
-  );
+    return (
+        // @ts-ignore
+        <RTCView ref={ref}
+            {...rtcViewProps}>
+            {fallbackView}
+        </RTCView>
+    );
 });
 
 export function startIOSPIP(ref) {
-  UIManager.dispatchViewManagerCommand(
-    ReactNative.findNodeHandle(ref.current),
-    // @ts-ignore
-    UIManager.RTCVideoView.Commands.startIOSPIP,
-    []
-  );
+    UIManager.dispatchViewManagerCommand(
+        ReactNative.findNodeHandle(ref.current),
+        UIManager.getViewManagerConfig('RTCVideoView').Commands.startIOSPIP,
+        []
+    );
 }
 
 export function stopIOSPIP(ref) {
-  UIManager.dispatchViewManagerCommand(
-    ReactNative.findNodeHandle(ref.current),
-    // @ts-ignore
-    UIManager.RTCVideoView.Commands.stopIOSPIP,
-    []
-  );
+    UIManager.dispatchViewManagerCommand(
+        ReactNative.findNodeHandle(ref.current),
+        UIManager.getViewManagerConfig('RTCVideoView').Commands.stopIOSPIP,
+        []
+    );
 }
 
 export default RTCPIPView;
