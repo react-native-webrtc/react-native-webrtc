2b43a4f5ef49251085c459825c1f876cc2f88941
Johnathon Weaver <weaver123_johnathon@hotmail.com>
2025-03-08 10:41:38 +0800

    Multiple video frame processors + implementation for ios

---
+++ ios/RCTWebRTC/videoEffects/VideoFrameProcessor.h
@@ -0,0 +1,9 @@
+#import <WebRTC/RTCVideoCapturer.h>
+#import <WebRTC/RTCVideoFrame.h>
+
+@protocol VideoFrameProcessorDelegate
+
+- (RTCVideoFrame *)capturer:(RTCVideoCapturer *)capturer
+      didCaptureVideoFrame:(RTCVideoFrame *)frame;
+
+@end
