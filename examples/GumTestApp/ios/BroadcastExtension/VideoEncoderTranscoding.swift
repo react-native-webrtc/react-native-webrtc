a29d87b8c88da171ce1abb756aec121cf11195be
Maksim Kalinin <maxim.kalinin.work@gmail.com>
2025-08-17 13:31:22 +0200

    Update example app with get display media example

---
+++ examples/GumTestApp/ios/BroadcastExtension/VideoEncoderTranscoding.swift
@@ -0,0 +1,31 @@
+import Foundation
+import CoreMedia
+import Transcoding
+
+class VideoEncoderTranscoding {
+  private let videoEncoder: VideoEncoder
+  private let videoEncoderAnnexBAdaptor: VideoEncoderAnnexBAdaptor
+
+  init() {
+    videoEncoder = VideoEncoder(config: .init(
+      codecType: kCMVideoCodecType_H264,
+      allowFrameReordering: false,
+      realTime: true,
+    ))
+    videoEncoderAnnexBAdaptor = VideoEncoderAnnexBAdaptor(
+      videoEncoder: videoEncoder
+    )
+  }
+
+  func encode(_ buffer: CMSampleBuffer) {
+    videoEncoder.encode(buffer)
+  }
+
+  var data: AsyncStream<Data> {
+    videoEncoderAnnexBAdaptor.annexBData
+  }
+  
+  func invalidate() {
+    videoEncoder.invalidate()
+  }
+}
