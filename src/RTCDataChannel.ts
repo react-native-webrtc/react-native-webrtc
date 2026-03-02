c43189ab2703ac790c720bf80f688314595fea30
wuxinfei <wuxinfei@bytedance.com>
2026-03-02 15:30:01 +0800

    refactor: replace defineEventAttribute with getter/setter methods for event attributes
    Updated MediaDevices, MediaStream, MediaStreamTrack, RTCDataChannel, and RTCPeerConnection classes to use getter and setter methods for event attributes instead of the defineEventAttribute function. This change enhances code readability and maintainability.

--- src/RTCDataChannel.ts
+++ src/RTCDataChannel.ts
@@ -1,5 +1,5 @@
 import * as base64 from 'base64-js';
-import { EventTarget, defineEventAttribute } from 'event-target-shim';
+import { EventTarget, getEventAttributeValue, setEventAttributeValue } from 'event-target-shim';
 import { NativeModules } from 'react-native';
 
 import { addListener, removeListener } from './EventEmitter';
@@ -22,7 +22,6 @@ type DataChannelEventMap = {
 export default class RTCDataChannel extends EventTarget<DataChannelEventMap> {
     _peerConnectionId: number;
     _reactTag: string;
-
     _bufferedAmount: number;
     _id: number;
     _label: string;
@@ -53,6 +52,54 @@ export default class RTCDataChannel extends EventTarget<DataChannelEventMap> {
         this._readyState = info.readyState;
 
         this._registerEvents();
+    }
+
+    get onbufferedamountlow() {
+        return getEventAttributeValue(this, 'bufferedamountlow');
+    }
+
+    set onbufferedamountlow(value) {
+        setEventAttributeValue(this, 'bufferedamountlow', value);
+    }
+
+    get onclose() {
+        return getEventAttributeValue(this, 'close');
+    }
+
+    set onclose(value) {
+        setEventAttributeValue(this, 'close', value);
+    }
+
+    get onclosing() {
+        return getEventAttributeValue(this, 'closing');
+    }
+
+    set onclosing(value) {
+        setEventAttributeValue(this, 'closing', value);
+    }
+
+    get onerror() {
+        return getEventAttributeValue(this, 'error');
+    }
+
+    set onerror(value) {
+        setEventAttributeValue(this, 'error', value);
+    }
+
+    get onmessage() {
+        return getEventAttributeValue(this, 'message');
+    }
+
+    set onmessage(value) {
+        setEventAttributeValue(this, 'message', value);
+    }
+
+    get onopen() {
+        return getEventAttributeValue(this, 'open');
+    }
+
+    set onopen(value) {
+        setEventAttributeValue(this, 'open', value);
     }
 
     get bufferedAmount(): number {
@@ -176,15 +223,3 @@ export default class RTCDataChannel extends EventTarget<DataChannelEventMap> {
         });
     }
 }
-
-/**
- * Define the `onxxx` event handlers.
- */
-const proto = RTCDataChannel.prototype;
-
-defineEventAttribute(proto, 'bufferedamountlow');
-defineEventAttribute(proto, 'close');
-defineEventAttribute(proto, 'closing');
-defineEventAttribute(proto, 'error');
-defineEventAttribute(proto, 'message');
-defineEventAttribute(proto, 'open');
